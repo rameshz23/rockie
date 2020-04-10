1323e8ce83722b3244236a3f42783b1cbecb2c10
ram <rameshz23@yahoo.com>
2020-04-10 18:46:29 +0530

    Update testJob.groovy

--- vars/testJob.groovy
+++ vars/testJob.groovy
@@ -34,14 +34,14 @@ def call(body) {
       script {
         currentBuild.result = 'SUCCESS'
       }
-       sparkSend ( credentialsId: 'sparkbot', disable: true, failOnError: true, message: '${JOB_NAME} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
+       sparkSend ( credentialsId: 'sparkbot',  message: '${JOB_NAME}-(${BUILD_NUMBER})-(${BUILD_RESULT})-${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
     }  
         
      failure {
       script {
         currentBuild.result = 'FAILURE'
       }
-   sparkSend ( credentialsId: 'sparkbot', disable: true, failOnError: true, message: '${JOB_NAME} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
+   sparkSend ( credentialsId: 'sparkbot',  message: '${JOB_NAME} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
      }
     }         
     } //eod
