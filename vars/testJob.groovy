d495de90b4a53a139897f589afeaa49e817299e5
ram <rameshz23@yahoo.com>
2020-04-10 22:33:07 +0530

    Update testJob.groovy

--- vars/testJob.groovy
+++ vars/testJob.groovy
@@ -26,25 +26,14 @@ def call(body) {
                         
                     }
                 }
+              post {
+    always {
+      noti currentBuild.result
+    }
+  }
           }
      }//eod 
-    post {
-    success {
-      script {
-        currentBuild.result = 'SUCCESS'
-      }
-      // sparkSend ( credentialsId: 'sparkbot',  message: '${JOB_NAME}-${BUILD_NUMBER}- ${BUILD_RESULT}- ${NODE_NAME}-${currentBuild.currentResult}-${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
-    sparkNotifyPostBuilder ( disable(false) , skipOnFailure(false) , skipOnSuccess(false) , skipOnAborted(false) , skipOnUnstable(false) , message: '[$JOB_NAME]($BUILD_URL)', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'Spark-messg']], credentialsId: 'webexbot')    
         
-    }  
-        
-     failure {
-      script {
-        currentBuild.result = 'FAILURE'
-      }
-   sparkSend ( credentialsId: 'sparkbot',  message: '${JOB_NAME}-${BUILD_NUMBER}-${currentBuild.currentResult} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
-     }
-    }     
           
         
         
