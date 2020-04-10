a1132037633c72eb80c03fa2f1b1968dd978c659
ram <rameshz23@yahoo.com>
2020-04-10 22:42:17 +0530

    Update noti.groovy

--- vars/noti.groovy
+++ vars/noti.groovy
@@ -24,5 +24,5 @@ def call(String buildStatus = 'STARTED') {
 
   //sparkSend ( credentialsId: 'sparkbot',  message: '${JOB_NAME}-${BUILD_NUMBER}-${currentBuild.currentResult} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )  
   
-   sparkSend ( credentialsId: 'sparkbot', color: '#FFFF00', message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})", messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
+   sparkSend ( credentialsId: 'sparkbot', color: '#FFFF00', message: "'${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})", messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
 }
