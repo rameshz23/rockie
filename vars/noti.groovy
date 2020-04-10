8961d667e65c82a5ddbffb422e3651170c1f71ba
ram <rameshz23@yahoo.com>
2020-04-10 22:40:07 +0530

    Update noti.groovy

--- vars/noti.groovy
+++ vars/noti.groovy
@@ -22,6 +22,7 @@ def call(String buildStatus = 'STARTED') {
     colorCode = '#FF0000'
   }
 
-  // Send notifications
-  sparkSend ( credentialsId: 'sparkbot',  message: '${JOB_NAME}-${BUILD_NUMBER}-${currentBuild.currentResult} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )  
+  //sparkSend ( credentialsId: 'sparkbot',  message: '${JOB_NAME}-${BUILD_NUMBER}-${currentBuild.currentResult} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )  
+  
+   sparkSend ( credentialsId: 'sparkbot', color: '#FFFF00', message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})", messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
 }
