1233d05ba4ab2675f8e57068c1f40bd04405ae6e
ram <rameshz23@yahoo.com>
2020-04-11 13:02:12 +0530

    Update noti.groovy

--- vars/noti.groovy
+++ vars/noti.groovy
@@ -1,6 +1,6 @@
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
-  //buildStatus = buildStatus ?: 'SUCCESS'
+  buildStatus = buildStatus ?: 'SUCCESS'
 if ( buildResult == "SUCCESS" ) {
  // old sparkSend ( credentialsId: 'sparkbot',  message: ' success - ${JOB_NAME}-${BUILD_NUMBER}-${currentBuild.currentResult} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )  
  sparkSend(  message: 'sucess - [$JOB_NAME]($BUILD_URL)', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'Spark-messg']], credentialsId: 'webexbot') 
