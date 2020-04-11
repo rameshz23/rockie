e606327f39afd9b3059a8842fdaeeea49bf68436
ram <rameshz23@yahoo.com>
2020-04-11 14:59:39 +0530

    Update noti.groovy

--- vars/noti.groovy
+++ vars/noti.groovy
@@ -2,7 +2,7 @@ def Details(status){
 //def call(String buildStatus = 'STARTED') {
   // build status of null means successful
  buildStatus = buildStatus ?: 'SUCCESS'
-  script{  
+   
 if ( buildStatus == "SUCCESS" ) {
  // old sparkSend ( credentialsId: 'sparkbot',  message: ' success - ${JOB_NAME}-${BUILD_NUMBER}-${currentBuild.currentResult} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )  
  sparkSend ( credentialsId: 'sparkbot',  message: ' success - ${JOB_NAME}-${BUILD_NUMBER} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] ) 
@@ -14,4 +14,4 @@ sparkSend ( credentialsId: 'sparkbot',  message: 'failure - ${JOB_NAME}-${BUILD_
  //color-  sparkSend ( credentialsId: 'sparkbot', color: '#FFFF00', message: "${env.JOB_NAME} - [${env.BUILD_NUMBER}] - (${env.BUILD_URL})", messageType: 'markdown', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']] )
 }
 
-}
+
