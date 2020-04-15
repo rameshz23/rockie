fe49a73f3db6261c44dc405334fc7d83473f852e
ram <rameshz23@yahoo.com>
2020-04-15 20:44:17 +0530

    Update testJob.groovy

--- vars/testJob.groovy
+++ vars/testJob.groovy
@@ -31,7 +31,7 @@ def call(body) {
                         echo env.USER
                       //  noti currentBuild.result
                        //noti(currentBuild.currentResult)
-                        echo ${DEPLOY_ENV}
+                        echo $DEPLOY_ENV
                         
                     }
                 }
